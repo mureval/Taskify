7f65354dc78361ec67d2e30698b5d0da4f2df9fe
mureval <muh.revan0801@gmail.com>
2024-10-20 15:28:55 +0700

    add a comment in index.js

--- src/index.js
+++ src/index.js
@@ -11,6 +11,7 @@ class App extends React.Component {
     return this.state.columnOrder.map(columnId => {
       const column = this.state.columns[columnId];
       const tasks = column.tasksIds.map(taskId => this.state.tasks[taskId])  
+      // test
 
       return <Column key={column.id} column={column} tasks={tasks} />;
     });
